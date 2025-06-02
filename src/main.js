815d93abc8d0e9ab23ee3bacbbc37151c8a6cda1
bismarko_nobilas <bismarko139@gmail.com>
2025-06-02 18:59:53 +0700

    innit repo

---
+++ src/main.js
@@ -0,0 +1,7 @@
+const Typed = require("typed.js");
+
+const typed = new Typed("#greeting", {
+  strings: ["Software Engineer", "Software Developer", "Fullstack Developer"],
+  loop: true,
+  typeSpeed: 60,
+});
